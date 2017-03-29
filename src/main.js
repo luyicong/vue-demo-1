48a457e28b3087960df25cfe3b404a2425a32957
LYC123 <980469887@qq.com>
2017-03-29 18:15:06 +0800

    页面基本架构

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import Vuex from 'Vuex'
 import App from './App'
 import router from './router'
-Vue.use(Vuex)
 Vue.config.productionTip = false
-let store = new Vuex.Store({})
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
+  template: '<App />',
   components: { App }
 })
